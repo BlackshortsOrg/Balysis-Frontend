5782da7c4997e1e7274f41e218bbbcccaeea2545
EsharkyTheGreat <eshwars1210@gmail.com>
2023-10-20 20:42:08 +0530

    Formatting

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,32 +1,50 @@
-import React from 'react'
+import React from "react";
 import { DropDownArrow } from "./svgs/DropDownArrow";
-import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@nextui-org/react";
-import { useState } from 'react';
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from "@nextui-org/react";
+import { useState } from "react";
 
-const DropDown = ({list}) => {
-    const menuList = list;
-    const [selectedMenuItem, setSelectedMenuItem]= useState("Metric");
+const DropDown = ({ list }) => {
+  const menuList = list;
+  const [selectedMenuItem, setSelectedMenuItem] = useState("Metric");
   return (
     <div>
-    <Dropdown className="bg-[#032D59]">
-      <DropdownTrigger>
-        <Button 
-          variant="bordered" style={{color:"#41AFFF", font:"bold", paddingRight:"20px", paddingLeft:"20px", borderColor:"#41AFFF"}} 
-        >
-          <span className='font-bold outline-none'>{selectedMenuItem}</span>
-        </Button>
-      </DropdownTrigger>
-      <DropdownMenu aria-label="Static Actions">
-        {
-            menuList.map(value=>{
-                return <DropdownItem key={value} className="text-[#41AFFF] bg-[#032D59] cursor-pointer px-4 py-2 rounded-lg" onClick={()=>setSelectedMenuItem(value)}>{value}</DropdownItem>
-            })
-        }
-       
-      </DropdownMenu>
-    </Dropdown>
+      <Dropdown className="bg-[#032D59]">
+        <DropdownTrigger>
+          <Button
+            variant="bordered"
+            style={{
+              color: "#41AFFF",
+              font: "bold",
+              paddingRight: "20px",
+              paddingLeft: "20px",
+              borderColor: "#41AFFF",
+            }}
+          >
+            <span className="font-bold outline-none">{selectedMenuItem}</span>
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu aria-label="Static Actions">
+          {menuList.map((value) => {
+            return (
+              <DropdownItem
+                key={value}
+                className="text-[#41AFFF] bg-[#032D59] cursor-pointer px-4 py-2 rounded-lg"
+                onClick={() => setSelectedMenuItem(value)}
+              >
+                {value}
+              </DropdownItem>
+            );
+          })}
+        </DropdownMenu>
+      </Dropdown>
     </div>
-  )
-}
+  );
+};
 
-export default DropDown
+export default DropDown;
