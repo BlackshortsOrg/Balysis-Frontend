f1d75a9096fd106e44c4120dfa7d002efb10cd3f
Krishan kant <krishankant@Krishans-MacBook-Air.local>
2023-10-24 15:10:05 +0530

    Footer improvisation in more succint order.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,58 +1,55 @@
-import React, { useState } from "react";
-import balysis from '../Assets/Group 7120.png'
-import { SocialIcon } from "react-social-icons";
-import { FaFacebook } from 'react-icons/fa'
-import { BsLinkedin } from 'react-icons/bs'
-import { BsInstagram } from 'react-icons/bs'
+import React from "react";
+import Icon from "../Assets/Icon";
+import { capitalize, keys, map, toLower } from "lodash";
+import { FOOTER_DATA } from "../Data/Footer";
 
-export default function Footer() {
-   const [x, setx] = useState(window.innerWidth / 60);
-   return (
-      <div className="footer">
-         <div className="footer-content">
-            <div className="footer-left">
-               <div className="footer-left-img" >
-                  <img src={balysis} alt='Balysis' />
-               </div>
-               <div className="footer-left-icons">
-                  <div className="icon1">
-                     <a href="https://www.instagram.com/sharnamagrawal_/"
-                     ><BsInstagram color="white" size={x} /> </a>
-                  </div>
-                  <div className="icon2"><a href="https://www.linkedin.com/company/blackshorts/" className="adjust-me"><BsLinkedin size={x}
-                     color="white" /></a></div>
-                  <div className="icon3"><a href="https://www.facebook.com/sharnam.agrawal.1/" className="adjust-me" ><FaFacebook size={x}
-                     color="white" /> </a></div>
-               </div>
-               <div className="footer-left-text">
-                  Copyright ©Blackshorts | All Rights Reserved 2023
-               </div>
-            </div>
-            <div className="footer-middle" style={{ marginTop: '10%' }}>
-               <div className="middle-heading" style={{ color: '#41AFFF', fontWeight: '700' }}>
-                  COMPANY
-               </div>
-               <div className="middle-content" style={{ marginTop: '6%', lineHeight: '170%', fontWeight: '500' }}>
-                  <p>Home</p>
-                  <p>About</p>
-                  <p>Privacy Policy</p>
-                  <p>Terms and Conditions</p>
-                  <p>Contact Us</p>
-               </div>
-            </div>
-            <div className="footer-end" style={{ marginTop: '10%' }}>
-               <div className="right-heading" style={{ color: '#41AFFF', fontWeight: '700' }}>
-                  RESOURCES
-               </div>
-               <div className="middle-content" style={{ marginTop: '6%', lineHeight: '170%', fontWeight: '500' }}>
-                  <p>Abhishek</p>
-                  <p>Rishita</p>
-                  <p>Aadeesh</p>
-                  <p>Eshwar</p>
-                  <p>Bahteti Secrets</p>
-               </div>
-            </div>
-         </div>
+const BalysisFooter = () => {
+  const footerDescription = keys(FOOTER_DATA);
+  return (
+    <div className="w-full p-4">
+      <div className="absolute">
+        <img src={Icon.balysisFooterLogo} />
+        <div className="flex flex-row ml-14 mt-3 gap-12">
+          <img src={Icon.facebook} className="w-7 h-7 cursor-pointer" />
+          <a
+            href="https://instagram.com/balysis?igshid=MzRlODBiNWFlZA=="
+            target="blank"
+          >
+            <img src={Icon.instagram} className="w-7 h-7 cursor-pointer" />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/blackshorts/"
+            target="blank"
+          >
+            <img src={Icon.linkedin} className="w-7 h-7 cursor-pointer" />
+          </a>
+        </div>
       </div>
-   )
-}
+      <div className="justify-between flex flex-row">
+        <div></div>
+        <div className="flex text-blue-balysisblue font-bold flex flex-row gap-52 pr-24 text-lg items-center mt-16">
+          {React.Children.toArray(
+            map(footerDescription, (description) => (
+              <div>
+                <div className="mb-3">{capitalize(toLower(description))}</div>
+                {React.Children.toArray(
+                  map(
+                    FOOTER_DATA[description],
+                    (individualFooterDescription) => (
+                      <a href={individualFooterDescription.link} target="blank">
+                        <div className="font-medium text-white text-base py-1">
+                          {individualFooterDescription.title}
+                        </div>
+                      </a>
+                    )
+                  )
+                )}
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default BalysisFooter;
